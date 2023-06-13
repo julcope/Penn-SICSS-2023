272187de565bfc9ebdd6771316274da84002c303
nanfengnfNanFengnf <nf263@cornell.edu>
2023-06-13 11:44:00 -0400

    this is a test of nan

--- nangit1.R
+++ nangit1.R
@@ -1,1 +1,30 @@
 ##Hey! This is my folders
+
+##get the working directory
+getwd()
+
+##move to a level of directory upward
+cd .. 
+
+##change directory to GitHub/Penn-SICSS-2023/
+cd  Penn-SICSS-2023/
+  
+##list the files in the folder
+ls
+
+## add the files in the current working directory to the git
+git add .
+
+## git pull
+git pull
+
+git push
+##git push
+
+##clear the working space
+clear
+
+##displays the name of the current directory
+pwd
+
+## change! 
